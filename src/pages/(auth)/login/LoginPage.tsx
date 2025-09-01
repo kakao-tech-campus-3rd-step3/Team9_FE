cba215951d9924e0d633b76c37dc5faf4bdd4499
SeHIgh <sehi0119@naver.com>
2025-09-01 23:44:25 +0900

    feat(auth): 로그인 페이지 UX 개선 및 코드 구조화
    - noValidate 속성 추가로 브라우저 기본 검증 팝업 방지
    - autoComplete 속성 적용으로 입력 UX 향상
    - React Hook Form 적용으로 폼 상태 관리 개선
    - 폴더 구조 분리 (types, constants, hooks, components)
    
    Related to #2

--- src/pages/(auth)/login/LoginPage.tsx
+++ src/pages/(auth)/login/LoginPage.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '@/constants';
 import { Logo } from '@/components';
-import { Input } from '../components';
 import { AUTH_TEXTS } from '../constants';
+import { LoginForm } from './components';
 
 /**
  * 로그인 페이지 컴포넌트
@@ -28,31 +28,7 @@ const LoginPage: React.FC = () => {
             </p>
           </div>
 
-          <form className='space-y-4'>
-            {/* 이메일 입력 */}
-            <Input
-              type='email'
-              id='email'
-              name='email'
-              placeholder={AUTH_TEXTS.LOGIN.EMAIL_PLACEHOLDER}
-            />
-
-            {/* 비밀번호 입력 */}
-            <Input
-              type='password'
-              id='password'
-              name='password'
-              placeholder={AUTH_TEXTS.LOGIN.PASSWORD_PLACEHOLDER}
-            />
-
-            {/* 로그인 버튼 */}
-            <button
-              type='submit'
-              className='w-full bg-primary text-primary-foreground py-2 px-3 mt-4 rounded hover:bg-primary-hover focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 transition-all duration-200 font-bold cursor-pointer'
-            >
-              {AUTH_TEXTS.LOGIN.BUTTON}
-            </button>
-          </form>
+          <LoginForm />
         </div>
 
         {/* 회원가입 링크 */}
