205e8a879dd0ed9a0624241c1073fcdbcbff832d
SeHIgh <sehi0119@naver.com>
2025-09-05 16:01:21 +0900

    feat(document): 문서 관리 페이지 export 및 react-toastify 의존성 추가
    Related to #9

---
+++ src/pages/(study)/document/index.ts
@@ -0,0 +1,7 @@
+export { default as DocumentPage } from './DocumentPage';
+export { default as DocumentAddPage } from './DocumentAddPage';
+export { default as DocumentDetailPage } from './DocumentDetailPage';
+export { default as DocumentEditPage } from './DocumentEditPage';
+export * from './types';
+export * from './constants';
+export * from './components';
