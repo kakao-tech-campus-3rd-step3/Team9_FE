fc5133c8edfb1acadacdfa25d213bf7a709eb6aa
Jeong In Jun <jijun897@naver.com>
2025-10-10 06:31:22 +0900

    feat(home): 스터디 클릭시 스터디 대시보드 페이지로 이동

--- src/pages/Home/components/StudyList.tsx
+++ src/pages/Home/components/StudyList.tsx
@@ -1,6 +1,8 @@
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import type { StudyMe } from '../types/study';
 import { studyColor } from '@/utils';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '@/constants';
 
 type StudyListProps = {
   studies: StudyMe[];
@@ -25,16 +27,21 @@ const StudyList = ({
         </button>
       )}
       {studies.map((study) => (
-        <div
+        <Link
           key={study.study_id}
-          className='flex text-center text-lg gap-4 px-4 py-2 items-center bg-blue-200 rounded-xl'
+          to={`${ROUTES.STUDY.DASHBOARD}?id=${study.study_id}`}
         >
           <div
-            className='min-w-5 min-h-5 rounded-full inline-block'
-            style={{ backgroundColor: studyColor(study.study_id) }}
-          />
-          <div className='font-medium line-clamp-1'>{study.title}</div>
-        </div>
+            key={study.study_id}
+            className='flex text-center text-lg gap-4 px-4 py-2 items-center bg-blue-200 rounded-xl'
+          >
+            <div
+              className='min-w-5 min-h-5 rounded-full inline-block'
+              style={{ backgroundColor: studyColor(study.study_id) }}
+            />
+            <div className='font-medium line-clamp-1'>{study.title}</div>
+          </div>
+        </Link>
       ))}
       {canRight && (
         <button onClick={onRight} className='text-lg font-medium'>
