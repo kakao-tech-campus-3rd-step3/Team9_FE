a866f842c59f90f7019206610a094bb829734711
Jeong In Jun <jijun897@naver.com>
2025-10-16 18:57:10 +0900

    refactor(home): SuspenseQuery를 사용하여 에러, 로딩 처리 일관화

--- src/pages/Home/components/StudyListSection.tsx
+++ src/pages/Home/components/StudyListSection.tsx
@@ -1,41 +1,36 @@
-import { LoadingSpinner } from '@/components';
+import { ErrorBoundary, LoadingSpinner } from '@/components';
 import { useStudyMeQuery } from '../hooks/useStudyMeQuery';
 import StudyList from './StudyList';
 import StudyNothing from './StudyNothing';
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 
 const StudyListSection = () => {
-  const { data: studies, isPending } = useStudyMeQuery();
+  const { data: studies = [] } = useStudyMeQuery();
   const [page, setPage] = useState(0);
   const pageSize = 3;
-
-  if (isPending || !studies) {
-    return (
-      <section className='flex flex-col w-full max-w-7xl gap-2'>
-        <h3 className='text-left font-bold mb-1'>나의 스터디</h3>
-        <LoadingSpinner />
-      </section>
-    );
-  }
 
   const totalPages = Math.ceil(studies.length / pageSize);
   const pagedStudies = studies.slice(page * pageSize, (page + 1) * pageSize);
 
   return (
-    <section className='flex flex-col w-full max-w-7xl gap-2'>
-      <h3 className='text-left font-bold mb-1'>나의 스터디</h3>
-      {studies.length > 0 ? (
-        <StudyList
-          studies={pagedStudies}
-          canLeft={page > 0}
-          onLeft={() => setPage(page - 1)}
-          canRight={page < totalPages - 1}
-          onRight={() => setPage(page + 1)}
-        />
-      ) : (
-        <StudyNothing />
-      )}
-    </section>
+    <ErrorBoundary>
+      <section className='flex flex-col w-full max-w-7xl gap-2'>
+        <h3 className='text-left font-bold mb-1'>나의 스터디</h3>
+        <Suspense fallback={<LoadingSpinner message='스터디 불러오는 중...' />}>
+          {studies.length > 0 ? (
+            <StudyList
+              studies={pagedStudies}
+              canLeft={page > 0}
+              onLeft={() => setPage(page - 1)}
+              canRight={page < totalPages - 1}
+              onRight={() => setPage(page + 1)}
+            />
+          ) : (
+            <StudyNothing />
+          )}
+        </Suspense>
+      </section>
+    </ErrorBoundary>
   );
 };
 
