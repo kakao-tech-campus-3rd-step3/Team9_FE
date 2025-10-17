5ff3047e4ab5af1f9ad97088cb5914efa05df1a5
Jeong In Jun <jijun897@naver.com>
2025-10-17 16:44:10 +0900

    fix(home): suspense 구조가 적용될 수 있도록 컴포넌트 분리

--- src/pages/Home/components/StudyListSection.tsx
+++ src/pages/Home/components/StudyListSection.tsx
@@ -4,40 +4,46 @@ import StudyList from './StudyList';
 import StudyNothing from './StudyNothing';
 import { Suspense, useState } from 'react';
 
-const LoadingSection = ({ message }: { message: string }) => {
+// 로딩 상태를 표시하는 컴포넌트
+const LoadingSection = () => {
   return (
     <div className='w-fit flex gap-6 bg-blue-100 border-2 border-primary rounded-lg px-6 py-4 items-center'>
       <div className='w-11 h-11 flex justify-center items-center'>
-        <LoadingSpinner message={message} />
+        <LoadingSpinner />
       </div>
     </div>
   );
 };
 
-const StudyListSection = () => {
-  const { data: studies = [] } = useStudyMeQuery();
+// 데이터 로딩 및 렌더링을 담당하는 새 컴포넌트
+const MyStudies = () => {
+  const { data: studies } = useStudyMeQuery();
   const [page, setPage] = useState(0);
   const pageSize = 3;
 
   const totalPages = Math.ceil(studies.length / pageSize);
   const pagedStudies = studies.slice(page * pageSize, (page + 1) * pageSize);
 
+  return studies.length > 0 ? (
+    <StudyList
+      studies={pagedStudies}
+      canLeft={page > 0}
+      onLeft={() => setPage(page - 1)}
+      canRight={page < totalPages - 1}
+      onRight={() => setPage(page + 1)}
+    />
+  ) : (
+    <StudyNothing />
+  );
+};
+
+const StudyListSection = () => {
   return (
     <ErrorBoundary>
       <section className='flex flex-col w-full max-w-7xl gap-2'>
         <h3 className='text-left font-bold mb-1'>나의 스터디</h3>
-        <Suspense fallback={<LoadingSection message='스터디 불러오는 중...' />}>
-          {studies.length > 0 ? (
-            <StudyList
-              studies={pagedStudies}
-              canLeft={page > 0}
-              onLeft={() => setPage(page - 1)}
-              canRight={page < totalPages - 1}
-              onRight={() => setPage(page + 1)}
-            />
-          ) : (
-            <StudyNothing />
-          )}
+        <Suspense fallback={<LoadingSection />}>
+          <MyStudies />
         </Suspense>
       </section>
     </ErrorBoundary>
