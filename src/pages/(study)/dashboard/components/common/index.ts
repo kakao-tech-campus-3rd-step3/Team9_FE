43d27833c4ba5676ccab444ebad0d494f624b91f
SeHIgh <sehi0119@naver.com>
2025-11-05 20:30:54 +0900

    feat(dashboard): 빈/로딩 공통 컴포넌트 추가
    Related to #82

--- src/pages/(study)/dashboard/components/common/index.ts
+++ src/pages/(study)/dashboard/components/common/index.ts
@@ -1,2 +1,4 @@
 export { default as SectionCard } from './SectionCard';
 export { default as StatItem } from './StatItem';
+export { default as DashboardEmpty } from './DashboardEmpty';
+export { default as SkeletonBlock } from './SkeletonBlock';
